dedd1714ebb744b82b1dfe8842a91ddc48d1a11c
Romulo Pereira <mp.romulo@gmail.com>
2021-01-25 18:36:18 -0300

    Adding routes/ Adding links to the Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './core/assets/styles/custom.scss'
-import './App.scss'
-import Navbar from './core/components/Navbar';
+import './core/assets/styles/custom.scss';
+import './App.scss';
+import Routes from './Routes';
 
 const App = () => {
     return (
-        <Navbar />
+        <Routes />
     );
 }
 
