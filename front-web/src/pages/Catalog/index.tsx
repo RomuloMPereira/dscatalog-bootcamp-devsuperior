2d17aaa89f037a8095404610fec85e037769516f
Rômulo Pereira <mp.romulo@gmail.com>
2021-01-29 19:57:42 -0300

    Refactoring the imports by setting a base url

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductsResponse } from '../../core/types/Product';
-import { makeRequest } from '../../core/utils/request'
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request'
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
