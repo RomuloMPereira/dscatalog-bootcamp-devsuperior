571139e7834fc3af90e1ce4d2810970ef08466b0
Rômulo Pereira <mp.romulo@gmail.com>
2021-02-09 14:42:03 -0300

    Custom ConstraintValidator (create an annotation to validate the user insertion

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/FieldMessage.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/FieldMessage.java
@@ -6,15 +6,15 @@ public class FieldMessage implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String fieldName;
-	private String fieldMessage;
+	private String message;
 	
 	public FieldMessage() {
 	}
 
-	public FieldMessage(String fieldName, String fieldMessage) {
+	public FieldMessage(String fieldName, String message) {
 		super();
 		this.fieldName = fieldName;
-		this.fieldMessage = fieldMessage;
+		this.message = message;
 	}
 
 	public String getFieldName() {
@@ -25,12 +25,12 @@ public class FieldMessage implements Serializable {
 		this.fieldName = fieldName;
 	}
 
-	public String getFieldMessage() {
-		return fieldMessage;
+	public String getMessage() {
+		return message;
 	}
 
-	public void setFieldMessage(String fieldMessage) {
-		this.fieldMessage = fieldMessage;
+	public void setFieldMessage(String message) {
+		this.message = message;
 	}
 	
 }
