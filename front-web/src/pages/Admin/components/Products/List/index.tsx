4fcfc27764552f95152770708498bf2dd06c4e6c
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-14 17:53:37 -0300

    Product list layout of Admin page

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './styles.scss';
+import Card from '../Card';
 
 const List = () => {
 
@@ -15,6 +16,11 @@ const List = () => {
             <button className="btn btn-primary btn-lg" onClick={handleCreate}>
                 ADICIONAR
             </button>
+            <div className="admin-list-container">
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     );
 }
