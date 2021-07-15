0589b3f98b5452851dfeb36877c4aa99d190bad4
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-15 15:47:34 -0300

    Adding deletion of a Product (making request DELETE)

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import './styles.scss';
 import Card from '../Card';
 import { ProductsResponse } from 'core/types/Product';
-import { makeRequest } from 'core/utils/request';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
 import Pagination from 'core/components/Pagination';
+import { toast } from 'react-toastify';
 
 const List = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
@@ -12,24 +13,43 @@ const List = () => {
     const [activePage, setActivePage] = useState(0);
     const history = useHistory();
 
-    useEffect(() => {
+    const getProducts = useCallback(() => {
         const params = {
             page: activePage,
             linesPerPage: 4,
             direction: 'DESC',
             orderBy: 'id'
-        }
+        };
 
         setIsLoading(true);
         makeRequest({ url: '/products', params })
             .then((response) => setProductsResponse(response.data))
             .finally(() => {
                 setIsLoading(false);
-            })
+            });
     }, [activePage]);
+
+    useEffect(() => {
+        getProducts();
+    }, [getProducts]);
 
     const handleCreate = () => {
         history.push('/admin/products/create');
+    }
+
+    const onRemove = (productId: number) => {
+        const confirm = window.confirm('Deseja realmente excluir este produto?');
+
+        if (confirm) {
+            makePrivateRequest({ url: `/products/${productId}`, method: 'DELETE' })
+                .then(() => {
+                    toast.info('Produto excluído com sucesso!');
+                    getProducts();
+                })
+                .catch(() => {
+                    toast.error('Erro ao excluir produto!');
+                });
+        }
     }
 
     return (
@@ -39,7 +59,7 @@ const List = () => {
             </button>
             <div className="admin-list-container">
                 {productsResponse?.content.map(product => (
-                    <Card product={product} key={product.id} />
+                    <Card product={product} key={product.id} onRemove={() => onRemove(product.id)} />
                 ))}
                 {productsResponse && (
                     <Pagination
