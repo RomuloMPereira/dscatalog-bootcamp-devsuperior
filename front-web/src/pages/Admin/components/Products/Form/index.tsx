50f0fff06d26dd373e27edc578d96aabe9a6893d
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-15 15:05:52 -0300

    Adapting the Product's form to handle register and edition (making requests PUT and POST)

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,24 +1,47 @@
-import { makePrivateRequest } from 'core/utils/request';
-import React from 'react';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
+import React, { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useForm } from 'react-hook-form';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 type FormState = {
     name: string;
     price: string;
     description: string;
-    imageUrl: string;
+    imgUrl: string;
+}
+
+type ParamsType = {
+    productId: string;
 }
 
 const Form = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<FormState>();
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm<FormState>();
     const history = useHistory();
+    const { productId } = useParams<ParamsType>();
+    const isEditing = productId !== 'create';
+    const formTitle = isEditing ? 'Editar um produto' : 'Cadastrar um produto';
+
+    useEffect(() => {
+        if (isEditing) {
+            makeRequest({ url: `/products/${productId}` })
+                .then(response => {
+                    setValue('name', response.data.name);
+                    setValue('price', response.data.price);
+                    setValue('description', response.data.description);
+                    setValue('imgUrl', response.data.imgUrl);
+                });
+        }
+    }, [productId, isEditing, setValue]);
 
     const onSubmit = (data: FormState) => {
-        makePrivateRequest({ url: '/products', method: 'POST', data })
+        makePrivateRequest({
+            url: isEditing ? `/products/${productId}` : '/products',
+            method: isEditing ? 'PUT' : 'POST',
+            data
+        })
             .then(() => {
                 toast.info('Produto salvo com sucesso!');
                 history.push('/admin/products');
@@ -30,7 +53,9 @@ const Form = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <BaseForm title="cadastrar um produto">
+            <BaseForm
+                title={formTitle}
+            >
                 <div className="row">
                     <div className="col-6">
                         <div className="margin-bottom-30">
@@ -67,15 +92,15 @@ const Form = () => {
                         </div>
                         <div className="margin-bottom-30">
                             <input
-                                {...register('imageUrl', { required: "Campo obrigatório" })}
-                                name="imageUrl"
+                                {...register('imgUrl', { required: "Campo obrigatório" })}
+                                name="imgUrl"
                                 type="text"
                                 className="form-control input-base"
                                 placeholder="Imagem do produto"
                             />
-                            {errors.imageUrl && (
+                            {errors.imgUrl && (
                                 <div className="invalid-feedback d-block">
-                                    {errors.imageUrl.message}
+                                    {errors.imgUrl.message}
                                 </div>
                             )}
                         </div>
