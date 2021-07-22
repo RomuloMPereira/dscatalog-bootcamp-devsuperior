f5517b7b60072496a2b10c2847bdcb19b39040c1
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-22 18:48:17 -0300

    Responsive Navbar in mobile screen

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
+import menu from '../../assets/images/menu.svg';
 import './styles.scss';
 
 const Navbar = () => {
     const [currentUser, setCurrentUser] = useState('');
     const location = useLocation();
+    const [drawerActive, setDrawerActive] = useState(false);
 
     useEffect(() => {
         const currentUserData = getAccessTokenDecoded();
@@ -25,27 +27,81 @@ const Navbar = () => {
             <Link to="/" className="nav-logo-text">
                 <h4>DS Catalog</h4>
             </Link>
+            <button
+                type="button"
+                className="menu-mobile-btn"
+                onClick={() => setDrawerActive(!drawerActive)}
+            >
+                <img src={menu} alt="Mobile Menu" />
+            </button>
 
-            <div className="menu-container">
+            <div className={drawerActive ? "menu-mobile-container" : "menu-container"}>
                 <ul className="main-menu">
                     <li>
-                        <NavLink to="/" activeClassName="active" exact className="nav-link">
+                        <NavLink
+                            to="/"
+                            activeClassName="active"
+                            exact
+                            className="nav-link"
+                            onClick={() => setDrawerActive(false)}
+                        >
                             HOME
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/products" activeClassName="active" className="nav-link">
+                        <NavLink
+                            to="/products"
+                            activeClassName="active"
+                            className="nav-link"
+                            onClick={() => setDrawerActive(false)}
+                        >
                             CATÁLOGO
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/admin" activeClassName="active" className="nav-link">
+                        <NavLink
+                            to="/admin"
+                            activeClassName="active"
+                            className="nav-link"
+                            onClick={() => setDrawerActive(false)}
+                        >
                             ADMIN
                         </NavLink>
                     </li>
+                    {drawerActive && (
+                        <li>
+                            {currentUser && (
+                                <a
+                                    href="#logout"
+                                    className="nav-link active d-inline"
+                                    onClick={(e) => {
+                                        setDrawerActive(false);
+                                        handleLogout(e);
+                                    }}
+                                >
+                                    {`LOGOUT - ${currentUser}`}
+                                </a>
+                            )}
+                        </li>
+                    )}
+                    {drawerActive && (
+                        <>
+                            {!currentUser && (
+                                <li>
+                                    <Link
+                                        to="/auth/login"
+                                        className="nav-link active"
+                                        onClick={() => setDrawerActive(false)}
+                                    >
+                                        LOGIN
+                                    </Link>
+                                </li>
+                            )}
+                        </>
+                    )}
                 </ul>
             </div>
-            <div className="text-right">
+            <div className="user-info-dnone text-right">
                 {currentUser && (
                     <>
                         {currentUser}
